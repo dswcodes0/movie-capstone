f72b1c3f72b4a3a60f1ef1662a637acbe205488c
Darren Washington <dsw@meta.com>
2023-07-05 13:39:12 -0700

    Added my files into folders for organization

---
+++ movie-capstone/src/App/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import SignedOutHome from "../SignedOutHome/SignedOutHome"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
