48a878433448bf8d868355ccff7590ae99911cb6
Darren Washington <dsw@meta.com>
2023-07-05 17:29:52 -0700

    add gitignore

--- movie-app/src/SignedOutHome/SignedOutHome.jsx
+++ movie-app/src/SignedOutHome/SignedOutHome.jsx
@@ -7,7 +7,8 @@ import SignUp from '../SignUp/SignUp'
 
 const SignedOutHome = () => {
     return (
-      <div>
+      <div className='main'>
+        <div className="navbar">
         <nav>
           <ul> 
             <li>
@@ -19,14 +20,17 @@ const SignedOutHome = () => {
           </ul>          
         </nav>
         <Routes>
-          <Route element={<SignIn/>} path="/SignIn">
-            
-          </Route>
-          <Route element={<SignUp/>} path="/SignOut">
-            
-          </Route>
+          <Route element={<SignIn/>} path="/SignIn"></Route>
+          <Route element={<SignUp/>} path="/SignOut"></Route>
         </Routes>
+        </div>
+        <div className="hero">
+          <h1>
+            
+          </h1>
+        </div>
       </div>
+      
 
     );
   };
