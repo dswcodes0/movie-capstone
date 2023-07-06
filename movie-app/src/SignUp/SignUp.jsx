647ca9c8bb96d88ae1e2ecfd86acbcd79d3dc4f3
Darren Washington <dsw@meta.com>
2023-07-06 12:02:55 -0700

    added html to sign up

--- movie-app/src/SignUp/SignUp.jsx
+++ movie-app/src/SignUp/SignUp.jsx
@@ -4,7 +4,14 @@ import './SignUp.css';
 const SignUp = () => {
     return (
       <div>
+        <h1>Unlock a world of enertainment!</h1>
+        <form>
+          <input type="text" name="username" placeholder='Username' />
+          <input type="email" name="email" placeholder='Email' />
+          <input type="password" name="password" placeholder='Password' />
+          <input type="submit" placeholder='Submit'/>
 
+        </form>
       </div>
     );
   };
