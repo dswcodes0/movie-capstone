132455771f850e666f0d2f1d452e8eaef07823f7
Darren Washington <dsw@meta.com>
2023-07-06 11:01:59 -0700

    finished html for SignedOutHome

--- movie-app/src/SignedOutHome/SignedOutHome.jsx
+++ movie-app/src/SignedOutHome/SignedOutHome.jsx
@@ -26,8 +26,26 @@ const SignedOutHome = () => {
         </div>
         <div className="hero">
           <h1>
-            hi
+          Welcome to the Watcher!
           </h1>
+          <div className='watcher'>
+            <img src="chatbot.png" alt="" />
+          <button>Ask the Watcher</button>
+          </div>
+          <div className='features'>
+              <div className="feature">
+                <h3>Recomendations</h3>
+                <p>Get personalized ecomendations based on your watch history</p>
+              </div>
+              <div className="feature">
+              <h3>Trailers</h3>
+              <p>Get HD trailers to any show you would like to view</p>
+              </div>
+              <div className="feature">
+              <h3>Saved Progress</h3>
+              <p>Don't worry about saving anything, it'll all be here, just browse, we do the rest.</p>
+              </div>
+          </div>
         </div>
       </div>
       
