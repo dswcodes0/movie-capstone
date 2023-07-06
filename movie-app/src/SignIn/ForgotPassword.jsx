524d7a19d7dcc50a7bcfe471d0b3cf9b30f81c95
Darren Washington <dsw@meta.com>
2023-07-06 11:40:10 -0700

    finished forgot password html

--- movie-app/src/SignIn/ForgotPassword.jsx
+++ movie-app/src/SignIn/ForgotPassword.jsx
@@ -4,6 +4,10 @@ const ForgotPassword = () => {
     return (
       <div>
         <h1>Forgot Password</h1>
+        <form>
+            <input type="email" placeholder='Enter your email'/>
+            <input type="submit" value="Reset Password" />
+        </form>
       </div>
     );
 };
