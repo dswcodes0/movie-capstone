1956c3db18d8a60ca7c86edf9321975e5a0b8720
Darren Washington <dsw@meta.com>
2023-07-06 21:32:16 -0700

    added signinhome html

--- movie-app/src/SignedInHome/SignedInHome.jsx
+++ movie-app/src/SignedInHome/SignedInHome.jsx
@@ -4,10 +4,31 @@ import './SignedInHome.css';
 const SignedInHome = () => {
     return (
       <div>
-          <nav>
-            
-          </nav>
+      <nav>
+        <img src="../chatbot.png" alt="" />
+        <input type="text" placeholder="Search..." />
+        <input type="submit" value="Submit" />
+      </nav>
+      <div className="chatbot">
+        <div className="chatbot-img">
+          <h1>How can I help?</h1>
+          <img src="../chatbot.png" alt="" />
+          <input type="text" placeholder="Ask me a question..." />
+          <input type="submit" value="Submit" />
+        </div>
       </div>
+      <div id="movies-grid" className="container">
+      {/* Placeholder for movie elements */}
+      </div>
+      <div id='recommendations' className="recommendations">
+        {/* Placeholder for recommended elements */}
+      </div>
+      <div id='watchlist' className="watchlist">
+        {/* Placeholder for watchlist/history elements */}
+      </div>
+
+      </div>
+      
     );
   };
   
