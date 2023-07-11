a04e816703be405bea8399a1fbba3f23d6dcd43d
Darren Washington <dsw@meta.com>
2023-07-11 10:25:39 -0700

    signedinhome fetches database info, displays users

--- movie-app/src/SignedInHome/SignedInHome.jsx
+++ movie-app/src/SignedInHome/SignedInHome.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SignedInHome.css';
 import Chatbot from '../Chatbot/Chatbot';
 import Watchlist from '../Watchlist/Watchlist';
 
 const SignedInHome = () => {
-    return (
-      <div>
+  const [users, setUsers] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const usersResponse = await fetch('http://localhost:3000/users');
+      const usersData = await usersResponse.json();
+      setUsers(usersData);
+
+      const moviesResponse = await fetch('http://localhost:3000/movies');
+      const moviesData = await moviesResponse.json();
+      setMovies(moviesData);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  }
+
+  return (
+    <div>
       <nav>
         <img src="../chatbot.png" alt="" />
         <input type="text" placeholder="Search..." />
         <input type="submit" value="Submit" />
       </nav>
-      <Chatbot/>
+      <Chatbot />
       <div id="movies-grid" className="container">
-      {/* Placeholder for movie elements */}
+        {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
       </div>
-      <div id='recommendations' className="recommendations">
+      <div id="recommendations" className="recommendations">
         {/* Placeholder for recommended elements */}
       </div>
-      <Watchlist/>
-      
+      <Watchlist />
+    </div>
+  );
+};
 
-      </div>
-      
-    );
-  };
-  
-  export default SignedInHome;
+export default SignedInHome;
