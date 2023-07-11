0bec7d06f4ed4c58c7ed7cbab03a9713cae4a77a
Darren Washington <dsw@meta.com>
2023-07-11 13:29:59 -0700

    added signin css and classnames

--- movie-app/src/SignIn/SignIn.jsx
+++ movie-app/src/SignIn/SignIn.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './SignIn.css';
-import ForgotPassword from './ForgotPassword'
+import ForgotPassword from './ForgotPassword';
 
 const SignIn = () => {
-    return (
-      <div>
-        <h1>Ready to binge?</h1>
-        <h3>Login</h3>
-        <form action="submit">
-          <input type="text" id='username' placeholder='Username'/>
-          <input type="password" id='password' placeholder='Password' />
-          <input type="submit" value="Submit"></input>
-        <Link to="/ForgotPassword">Forgot Password?</Link>
-        </form>
-      </div>
-    );
-  };
-  
-  export default SignIn;
+  return (
+    <div className="container">
+      <h1 className="title-signin">Ready to binge?</h1>
+      <h3 className="subtitle">Login</h3>
+      <form action="submit" className="signin-form">
+        <input type="text" id="username" placeholder="Username" className="input-field" />
+        <input type="password" id="password" placeholder="Password" className="input-field" />
+        <input type="submit" value="Submit" className="submit-btn" />
+        <Link to="/ForgotPassword" className="forgot-password-link">Forgot Password?</Link>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
