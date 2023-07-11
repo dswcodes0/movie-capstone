e7da084bdd4e0a2a100b5c974e882fe9e5ceaab5
Darren Washington <dsw@meta.com>
2023-07-11 13:37:16 -0700

    solved conflicting css throughout components

--- movie-app/src/SignedInHome/SignedInHome.jsx
+++ movie-app/src/SignedInHome/SignedInHome.jsx
@@ -5,7 +5,7 @@ import Watchlist from '../Watchlist/Watchlist';
 
 const SignedInHome = () => {
   const [users, setUsers] = useState([]);
-  const [movies, setMovies] = useState([]);
+  const [anime, setAnime] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -17,9 +17,9 @@ const SignedInHome = () => {
       const usersData = await usersResponse.json();
       setUsers(usersData);
 
-      const moviesResponse = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=eed0e2d8cf492449f59a2b5688eca30b');
-      const moviesData = await moviesResponse.json();
-      setMovies(moviesData.results);
+      const animeResponse = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=eed0e2d8cf492449f59a2b5688eca30b&with_genres=16');
+      const animeData = await animeResponse.json();
+      setAnime(animeData.results);
     } catch (error) {
       console.log('Error fetching data:', error);
     }
@@ -27,24 +27,24 @@ const SignedInHome = () => {
 
   return (
     <div>
-      <nav>
+      <header>
         <img src="../chatbot.png" alt="" />
         <input type="text" placeholder="Search..." />
         <input type="submit" value="Submit" />
-      </nav>
+      </header>
       <Chatbot />
-      <div id="movies-grid" className="container">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <div className="movie-info">
-              <h3>{movie.title}</h3>
-              <p>Rating: {movie.vote_average}</p>
+      <div className="anime-grid">
+        {anime.map((anime) => (
+          <div key={anime.id} className="anime-card">
+            <img src={`https://image.tmdb.org/t/p/w500${anime.poster_path}`} alt={anime.title} />
+            <div className="anime-info">
+              <h3>{anime.title}</h3>
+              <p>Rating: {anime.vote_average}</p>
             </div>
           </div>
         ))}
       </div>
-      <div id="recommendations" className="recommendations">
+      <div className="recommendations">
         {/* Placeholder for recommended elements */}
       </div>
       <Watchlist />
